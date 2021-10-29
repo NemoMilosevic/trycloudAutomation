20a68bacb5902f20cc2223a7355aeae1b57e844c
Nemanja M <nemanja985@yahoo.com>
2021-10-29 09:27:54 -0400

    US_10 Step_Defs fixed

--- src/test/java/com/trycloud/step_definitions/US10_FilesModuleStepDefs.java
+++ src/test/java/com/trycloud/step_definitions/US10_FilesModuleStepDefs.java
@@ -7,10 +7,29 @@ import com.trycloud.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
-public class US10_FileUploadStepDefs {
+public class US10_FilesModuleStepDefs {
 
+    SettingsPage settingsPage = new SettingsPage();
     FileUploadPage fileUploadPage = new FileUploadPage();
+
+    @When("user click Settings on the left bottom corner")
+    public void userClickSettingsOnTheLeftBottomCorner() {
+        settingsPage.filesModule.click();
+        settingsPage.settingsButton.click();
+    }
+
+    @Then("verify user can click any buttons")
+    public void verifyUserCanClickAnyButtons() {
+        BrowserUtils.waitFor(2);
+        boolean beforeSelect = settingsPage.checkBoxesValue.get(2).isSelected();
+        settingsPage.checkBoxesShow.get(2).click();
+        BrowserUtils.waitFor(2);
+        boolean afterSelect = settingsPage.checkBoxesValue.get(2).isSelected();
+
+        Assert.assertTrue(!beforeSelect==afterSelect);
+    }
 
     @When("user check the current storage usage")
     public void userCheckTheCurrentStorageUsage() {
